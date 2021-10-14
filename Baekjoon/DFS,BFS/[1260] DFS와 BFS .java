21d4f2f0cac7c18ad07609096947ba87c760b040
tlsrbwls999 <55525916+tlsrbwls999@users.noreply.github.com>
2021-10-14 22:30:19 +0900

    Update [1260] DFS와 BFS .java

--- Baekjoon/DFS,BFS/[1260] DFS와 BFS .java
+++ Baekjoon/DFS,BFS/[1260] DFS와 BFS .java
@@ -63,3 +63,57 @@ public class Main {
         }
     }
 }
+
+//bfs식 풀이
+
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    static int[][] check;
+    static boolean[] checked;
+    static int n;
+    static int m;
+    
+    public static void main(String[] args) throws IOException{
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+        
+        check=new int[n+1][n+1];
+        checked=new boolean[n+1];
+        
+        for(int i=0;i<m;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            check[a][b]=check[b][a]=1;
+        }
+        
+        
+        bfs();
+        int cnt=0;
+        for(int k=1;k<=n;k++){
+            if(checked[k]==true){
+                cnt++;
+                }
+            }
+        System.out.println(cnt-1);
+    }
+    
+    public static void bfs(){
+        Queue<Integer>queue=new LinkedList<>();
+        queue.offer(1);
+        checked[1]=true;
+        
+        while(!queue.isEmpty()){
+            int temp=queue.poll();
+            for(int i=1;i<=n;i++){
+                if(check[temp][i]==1 &&checked[i]==false){
+                    queue.offer(i);
+                    checked[i]=true;
+                }
+            }
+        }
+    }
+}
